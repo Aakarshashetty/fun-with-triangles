0dbbb48bd2b5c0bb94d2b89559a548378a8ded58
Aakarshashetty <aakarshakodthiwada61121@gmail.com>
2022-08-13 22:41:19 +0530

    added isTriangle and quiz logic

--- scripts/isTriangle.js
+++ scripts/isTriangle.js
@@ -1,1 +1,19 @@
-console.log("hi");
+const angleInput = document.querySelectorAll(".angle-input");
+const btn = document.querySelector(".btn");
+const output = document.querySelector(".output");
+
+const calculateSum = (angle1, angle2, angle3)=>{
+    let sum = angle1+angle2+angle3;
+    console.log(sum)
+    return sum;
+}
+const isTriangle = () =>{
+    const sumOfTriangles = calculateSum(Number(angleInput[0].value), Number(angleInput[1].value), Number(angleInput[2].value));
+    if(sumOfTriangles === 180){
+        output.innerText = "Yess It is a Triangle🔺";
+    }else{
+        output.innerText = "Oops! It is not a Triangle😐";
+    }
+}
+btn.addEventListener("click",isTriangle);
+    
--- scripts/quiz.js
+++ scripts/quiz.js
@@ -1,1 +1,25 @@
-console.log("hi");
+const crtAns = ["90°", "right angled", "one right angle", "12, 16, 20", "Equilateral triangle", "100°", "30°", "a + b + c", "no", "45°"];
+const form = document.querySelector(".form");
+const btn = document.querySelector(".submit-btn");
+const output = document.querySelector(".output");
+
+let score = 0;
+const calculateScore = () => {
+    //console.log("calc score");
+    const userAns = [form.question1.value, form.question2.value, form.question3.value, form.question4.value, form.question5.value, form.question6.value, form.question7.value, form.question8.value, form.question9.value, form.question10.value]
+    userAns.forEach((answer, index) => {
+        if(answer === crtAns[index]){
+            score++;
+        }
+        console.log(score);
+        displayUI(score);
+    });
+};
+const displayUI = (score)=>{
+    let html = `Your score is ${score}`;
+    output.innerText = html;
+};
+btn.addEventListener("click", e=>{
+    e.preventDefault();
+    calculateScore();
+});
