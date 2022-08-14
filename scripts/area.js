242624afb4f8a75aa7c24791e5fa42b7d583ca06
Aakarshashetty <aakarshakodthiwada61121@gmail.com>
2022-08-14 19:43:50 +0530

    added area logic

--- scripts/area.js
+++ scripts/area.js
@@ -1,1 +1,17 @@
-console.log("hi");
+const sides = document.querySelectorAll(".side-input");
+const calcBtn = document.querySelector(".calc-btn");
+const output = document.querySelector(".output");
+
+const calculateArea = () => {
+    if(sides[0].value && sides[1].value !== null){
+        const res = (sides[0].value * sides[1].value)/2;
+       // console.log(res);
+        displayUI(res);
+    }else{
+        output.innerText = "please enter valid input";
+    }
+};
+const displayUI = res=>{
+    output.innerText = `Area of the triangle is ${res}`;
+};
+calcBtn.addEventListener("click", calculateArea);
