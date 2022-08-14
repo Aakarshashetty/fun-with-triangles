0197db191cb601c67c7c3d8d181290920a70c88f
Aakarshashetty <aakarshakodthiwada61121@gmail.com>
2022-08-14 19:04:30 +0530

    added hypotenuse logic

--- scripts/hypotenuse.js
+++ scripts/hypotenuse.js
@@ -1,1 +1,18 @@
-console.log("hi");
+const sides = document.querySelectorAll(".side-input");
+const calcBtn = document.querySelector(".calculate");
+const output = document.querySelector(".output");
+
+const claculateHypotenuse = ()=>{
+//    console.log(sides[0].value**2,sides[1].value**2);
+    if(sides[0].value && sides[1].value !== null){
+        const res =  (Math.sqrt((sides[0].value ** 2) + (sides[1].value**2)));
+    //console.log(res);
+        displayUI(res);
+    }else{
+        output.innerText = "Please Enter valid Input😐";
+    }
+};
+const displayUI = res => {
+    output.innerText = `The length of hypotenuse is ${res}🙂`;
+};
+calcBtn.addEventListener("click", claculateHypotenuse);
